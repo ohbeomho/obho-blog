70ab8fd66d15bee680993cdde5f9e4d873d32deb
OhBeomho <tyler0322@naver.com>
2023-07-16 15:47:34 +0900

    new blog: coloring-text

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -1,38 +1,38 @@
-const BLOG_COUNT = 3;
+const BLOG_COUNT = 4
 
 export type Blog = {
   attributes: {
-    title: string;
-    writeDate: string;
-  };
-  content: any;
-  id: number;
-};
+    title: string
+    writeDate: string
+  }
+  content: any
+  id: number
+}
 
 export async function getBlog(id: number): Promise<Blog> {
-  const { attributes, default: content } = await import(`../blogs/${id}.md`);
-  return { attributes, content, id };
+  const { attributes, default: content } = await import(`../blogs/${id}.md`)
+  return { attributes, content, id }
 }
 
 export async function getRecentBlogs(): Promise<Blog[]> {
-  const end = BLOG_COUNT >= 3 ? BLOG_COUNT - 3 : 0;
-  const promises = [];
+  const end = BLOG_COUNT >= 3 ? BLOG_COUNT - 3 : 0
+  const promises = []
 
   for (let i = BLOG_COUNT - 1; i >= end; i--) {
-    promises.push(getBlog(i));
+    promises.push(getBlog(i))
   }
 
-  const blogs = await Promise.all(promises);
-  return blogs;
+  const blogs = await Promise.all(promises)
+  return blogs
 }
 
 export async function getBlogs(): Promise<Blog[]> {
-  const promises = [];
+  const promises = []
 
   for (let i = BLOG_COUNT - 1; i >= 0; i--) {
-    promises.push(getBlog(i));
+    promises.push(getBlog(i))
   }
 
-  const blogs = await Promise.all(promises);
-  return blogs;
+  const blogs = await Promise.all(promises)
+  return blogs
 }
